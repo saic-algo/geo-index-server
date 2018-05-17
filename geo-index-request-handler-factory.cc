4442dd20723d5fcc7b74e9ced5a6622a7df73521
Wei Wei <shazhouke@gmail.com>
2018-05-17 03:25:29 +0000

    Handle post

--- geo-index-request-handler-factory.cc
+++ geo-index-request-handler-factory.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "geo-index-request-handler-factory.h"
 #include "geo-index-request-handler.h"
 #include "request-handlers/create-index.h"
@@ -5,11 +7,9 @@
 HTTPRequestHandler* GeoIndexRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request) {
   if (request.getURI() == "/") 
     return new GeoIndexRequestHandler();
-  if (request.getURI() == "/create-index")
+  if (request.getURI() == "/create-index" && request.getMethod() == HTTPServerRequest::HTTP_POST)
     return new CreateIndexRequestHandler();
   else
     return 0;
 }
-
-
 
--- request-handlers/create-index.cc
+++ request-handlers/create-index.cc
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <memory>
 #include "create-index.h"
 
+using std::istream;
 using std::ostream;
 using std::endl;
+using std::make_unique;
 
 void CreateIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPServerResponse &response) {
   response.setChunkedTransferEncoding(true);
   response.setContentType("text");
 
+  istream &istm = request.stream();
+  size_t length = request.getContentLength();
+  auto buffer =  make_unique<char[]>(length);
+  istm.read(buffer.get(), length);
+  std::string content(buffer.get(), length);
+
   ostream &ostr = response.send();
-  ostr << "Create Index" << endl;
+  ostr << content << endl;
 }
 
