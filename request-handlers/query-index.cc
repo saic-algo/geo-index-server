bab671853515107300572a15fb9e8244e60039a1
Yuanxiang Ying(yuyi) <Yuanxiang.Ying@microsoft.com>
2018-05-29 11:23:08 +0800

    use poco thread library

--- request-handlers/query-index.cc
+++ request-handlers/query-index.cc
@@ -18,17 +18,46 @@ using std::vector;
 using Poco::JSON::Object;
 using Poco::JSON::Array;
 
-class HelloRunnable : public Poco::Runnable
+class QueryRunnable : public Poco::Runnable
 {
-    static int id;
-    virtual void run()
-    {
-        id++;
-        std::cout << "Hello, runnable world num " << id << "!" << std::endl;
-    }
+public:
+  static int id;
+  QueryRunnable(const GeoIndex *pIndex, Array::Ptr targets, int i)
+  : pIndex(pIndex), targets(targets), i(i) { 
+    id = id+1
+    std::cout << "Create QueryRunnable " << id << std::endll;
+  }
+
+  virtual void run()
+  {
+    Object::Ptr result(new Object);
+    Array::Ptr points(new Array);
+
+    auto gg = targets->get(i);
+    Array::Ptr targetPoint = gg.extract<Array::Ptr>();
+
+    const string &id = targetPoint->get(0).toString();
+    const double lat = (double)targetPoint->get(1);
+    const double lng = (double)targetPoint->get(2);
+
+    auto pPoints = pIndex->QueryClosestPoints(GeoPoint(id, lat, lng), count, radius);
+
+    // for (auto &point: *pPoints) {
+    //   points->add((const Array::Ptr)point);
+    // }
+
+    // result->set("points", points);
+
+    // tempResults[i] = result;
+  }
+
+private:
+  const GeoIndex *pIndex;
+  const Array::Ptr targets;
+  int i;
 };
 
-int HelloRunnable::id = 0;
+int QueryRunnable::id = 0;
 
 void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPServerResponse &response) {
   // Find the index
@@ -58,7 +87,7 @@ void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPSer
 
   std::vector<Poco::Thread> threads(10);
   for (auto& thread : threads) {
-      HelloRunnable hello;
+      QueryRunnable hello(pIndex, targets, i);
       thread.start(hello);
   }
 
@@ -66,40 +95,6 @@ void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPSer
       thread.join();
   }
 
-  omp_set_num_threads(10);
-  std::cout << "Max num threads: " << omp_get_max_threads() << ", Num threads: " << omp_get_num_threads() << std::endl;
-  
-  int num_query = targets->size();
-
-#pragma omp parallel for num_threads(10)
-  for (int i=0; i<num_query; ++i){
-    
-    std::cout << "Max num threads: " << omp_get_max_threads() << ", Num threads: " << omp_get_num_threads() << ", thread id" << omp_get_thread_num() << std::endl;
-/*    Object::Ptr result(new Object);
-    Array::Ptr points(new Array);
-
-    auto gg = targets->get(i);
-    Array::Ptr targetPoint = gg.extract<Array::Ptr>();
-
-    const string &id = targetPoint->get(0).toString();
-    const double lat = (double)targetPoint->get(1);
-    const double lng = (double)targetPoint->get(2);*/
-/*
-    auto pPoints = pIndex->QueryClosestPoints(GeoPoint(id, lat, lng), count, radius);
-
-    for (auto &point: *pPoints) {
-      points->add((const Array::Ptr)point);
-    }
-
-    result->set("points", points);
-
-    tempResults[i] = result;*/
-  }
-/*
-  for(auto& result : tempResults){
-    results->add(result);
-  }
-*/
   for (auto &i: *targets) {
     Object::Ptr result(new Object);
     Array::Ptr points(new Array);
