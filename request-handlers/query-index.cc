ce4ad6de811be8fe832c7310b95bf101a26f57dc
Yuanxiang Ying <Yuanxiang.Ying@microsoft.comw>
2018-05-31 06:59:51 +0000

    thread wise perf test, using std::trhead

--- request-handlers/query-index.cc
+++ request-handlers/query-index.cc
@@ -2,6 +2,7 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <regex>
 #include <thread>
 #include <mutex>
@@ -28,22 +29,20 @@ Array::Ptr targets;
 Array::Ptr results;
 double radius;
 int count;
+std::vector<std::string> pids;
+std::vector<double> lats;
+std::vector<double> lngs;
+std::vector<std::ostringstream> threadLogs;
 
 void batchProcessQuery(int id, int start, int end)
 {
-	float startTime = (float)clock()/CLOCKS_PER_SEC;
+  float startTime = (float)clock()/CLOCKS_PER_SEC;
   for(int i=start; i<end; ++i)
   {
     Object::Ptr result(new Object);
     Array::Ptr points(new Array);
-
-    Array::Ptr targetPoint = targets->get(i).extract<Array::Ptr>();
-
-    const string &id = targetPoint->get(0).toString();
-    const double lat = (double)targetPoint->get(1);
-    const double lng = (double)targetPoint->get(2);
     
-    auto pPoints = pIndex->QueryClosestPoints(GeoPoint(id, lat, lng), count, radius);
+    auto pPoints = pIndex->QueryClosestPoints(GeoPoint(pids[i], lats[i], lngs[i]), count, radius);
 
     for (auto &point: *pPoints) {
       points->add((const Array::Ptr)point);
@@ -56,8 +55,9 @@ void batchProcessQuery(int id, int start, int end)
     // results->add(result);
     // mtx.unlock();
   }
-	float endTime = (float)clock()/CLOCKS_PER_SEC;
-	std::cout << "Thread " << id << " starts at " << startTime << ", end at " << endTime << ", duration " << endTime - startTime << " seconds." <<std::endl;
+
+  float endTime = (float)clock()/CLOCKS_PER_SEC;
+  threadLogs[id] << "Thread " << id << " processes queries[" << start << "," << end << "). It starts at " << startTime << ", end at " << endTime << ", duration " << endTime - startTime << " seconds.";
 }
 
 void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPServerResponse &response) {
@@ -85,14 +85,31 @@ void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPSer
   m_performanceLogger.start("make-query");
 
   int num_query = targets->size();
-  int num_threads = 4;
+  int num_threads = 64;
   num_threads = num_query > num_threads ? num_threads : num_query;
-  
+  threadLogs.clear();
+  threadLogs.resize(num_threads);
+
   int batch_size = (num_query + num_threads - 1) / num_threads;
 
   tempResults = std::vector<Object::Ptr>(num_query);
+  pids.resize(num_query);
+  lats.resize(num_query);
+  lngs.resize(num_query);
 
-  std::cout <<"num_threads: " << num_threads << ", num_query: " << num_query << ", batch_size: " << batch_size << std::endl;
+  int index = 0;
+  m_performanceLogger.start("parsing-query");
+  for (auto &i: *targets) {
+    Array::Ptr targetPoint = i.extract<Array::Ptr>();
+
+    pids[index] = targetPoint->get(0).toString();
+    lats[index] = (double)targetPoint->get(1);
+    lngs[index] = (double)targetPoint->get(2);
+    ++index;
+  }
+  m_performanceLogger.finish("parsing-query");
+
+  std::cout <<"num_threads: " << num_threads << ", num_query: " << num_query << ", batch_size: " << batch_size << "\n";
 
   std::vector<std::thread> threads;
 
@@ -122,7 +139,11 @@ void QueryIndexRequestHandler::handleRequest(HTTPServerRequest &request, HTTPSer
   objRes->set("results", results);
 
   for (auto &pair: m_performanceLogger) {
-    std::cout << pair.first << " takes " << pair.second << " seconds" << std::endl;
+    std::cout << pair.first << " takes " << pair.second << " seconds\n";
+  }
+
+  for(auto &ostr : threadLogs){
+    std::cout << ostr.str() << "\n";
   }
 
   SendResponse(response, objRes);
